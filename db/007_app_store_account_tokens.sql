5d16bb747c80491ecfb6421de4ff43bb90e3281a
zm56snbdvp-hash <zm56snbdvp@privaterelay.appleid.com>
2026-09-04 03:42:11 +0200

    Remove user-wide StoreKit token table

--- db/007_app_store_account_tokens.sql
+++
@@ -1,6 +0,0 @@
--- Stable server-issued StoreKit appAccountToken per BirdieWorld user.
-CREATE TABLE IF NOT EXISTS moment_app_store_account_tokens (
-  user_id TEXT PRIMARY KEY,
-  app_account_token TEXT NOT NULL UNIQUE,
-  created_at TEXT NOT NULL
-);
